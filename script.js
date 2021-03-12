ea090012f65abfdfd6be1c34a388432c716b3741
pdxbellasaurus <74746211+pdxbellasaurus@users.noreply.github.com>
2021-03-11 19:16:37 -0800

    update img location

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ $( "#pastSearches").on("click", ".past", function(event) {
         sign = sunSign(month, day);
         signEl.textContent = sign;
         signImg = sign + ".png"
-        symbolEl.setAttribute("src", "assets/" + signImg);
+        symbolEl.setAttribute("src", "./assets/" + signImg);
         var url = 'https://aztro.sameerkumar.website/?sign=' + sign + '&day=today';
         fetch(url, {method: "POST"})
         .then(response => response.json())
